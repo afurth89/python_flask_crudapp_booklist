0e18136a99dfb8f4d6af52598a19e14dd8818331
Andrew Furth <andrew.furth@gmail.com>
2016-06-23 19:15:50 -0700

    PATCH and DELETE functions working

--- app.py
+++ app.py
@@ -33,14 +33,36 @@ def show_book(id):
   return redirect(url_for('books'))
 
 @app.route('/books/<int:id>', methods=["DELETE"])
-def delete_book():
+def delete_book(id):
+  for book in book_list:
+    if book.id == id:
+      book_list.remove(book)
+      return redirect(url_for('books'))
+    else:
+      pass
   return redirect(url_for('books'))
 
 
 @app.route('/books/<int:id>/edit', methods=['GET'])
-def edit_book():
-  return render_template('edit.html')
+def edit_book(id):
+  for book in book_list:
+    if book.id == id:
+      selected_book = book
+      return render_template('edit.html', book=selected_book)
+    else:
+      pass
+  return redirect(url_for('books'))
 
+@app.route('/books/<int:id>', methods=['PATCH'])
+def update_book(id):
+  for book in book_list:
+    if book.id == id:
+      book.title = request.form['title']
+      book.author = request.form['author']
+      return redirect(url_for('show_book', id=id))
+    else:
+      pass
+  return redirect(url_for('show_book', id=id))
 if __name__ == '__main__':
   app.run(debug=True, port=3000)
 
