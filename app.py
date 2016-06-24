4a0d8b8d7975615699d2fd8b5f1da5b63e37413a
Andrew Furth <andrew.furth@gmail.com>
2016-06-23 17:56:10 -0700

    GET routing complete for Index,New,Show,Edit

--- app.py
+++ app.py
@@ -8,6 +8,18 @@ book_list = []
 def index():
   return render_template('index.html')
 
+@app.route('/books/new', methods=['GET'])
+def new_book():
+  return render_template('new.html')
+
+@app.route('/books/<int:id>', methods=['GET'])
+def show_book():
+  return render_template('show.html')
+
+@app.route('/books/<int:id>/edit', methods=['GET'])
+def edit_book():
+  return render_template('edit.html')
+
 if __name__ == '__main__':
   app.run(debug=True, port=3000)
 
